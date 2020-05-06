50e69341b7de583d1e1892194ad7968c3b853969
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-05-06 01:23:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,43 @@
-class Artist 
-attr_accessor :name 
-attr_reader :songs 
-@@all = []
+class Artist
+  extend Concerns::Findable
 
-def initialize(name)
-  @name = name
-  @songs = []
-end
+  attr_accessor :name
+  attr_reader :songs
 
-def self.all
-  @@all 
-end
+  @@all = []
 
-def save 
-  self.class.all << self
-end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-def self.destroy_all 
-  @@all.clear
-end
+  def self.all
+    @@all
+  end
 
-def self.create(name)
-  artist = Artist.new(name)
-  artist.save
-  artist
-end
+  def self.destroy_all
+    all.clear
+  end
 
-def add_song(song)
-  song.artist = self unless song.artist
-  @song << song unless songs.include?(song)
-end
+  def save
+    self.class.all << self
+  end
 
+  def self.create(name)
+    artist = new(name)
+    artist.save
+    artist
+
+    # Or, as a one-liner:
+    # new(name).tap{ |a| a.save }
+  end
+
+  def add_song(song)
+    song.artist = self unless song.artist
+    songs << song unless songs.include?(song)
+  end
+
+  def genres
+    songs.collect{ |s| s.genre }.uniq
+  end
 end
